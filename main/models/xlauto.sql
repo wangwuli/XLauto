ebeca97543ab831ae5c8c6faa9d6b78c422f9956
wuli.wang <282621346@qq.com>
2020-05-05 23:37:35 +0800

    noissue;主机信息新增TCP信息

--- main/models/xlauto.sql
+++ main/models/xlauto.sql
@@ -23,18 +23,36 @@ CREATE TABLE IF NOT EXISTS `host_instance` (
   `host_id` int(11) NOT NULL AUTO_INCREMENT,
   `host_ip` varchar(50) NOT NULL,
   `host_name` varchar(50) DEFAULT NULL,
-  `host_type` varchar(50) NOT NULL,
+  `host_port` int(6) DEFAULT NULL,
+  `host_type_key` varchar(50) NOT NULL,
   `host_project` varchar(50) DEFAULT NULL,
   `is_remove` int(11) DEFAULT NULL,
   `comment` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`host_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- 正在导出表  xlauto.host_instance 的数据：~0 rows (大约)
+-- 正在导出表  xlauto.host_instance 的数据：~1 rows (大约)
 /*!40000 ALTER TABLE `host_instance` DISABLE KEYS */;
-INSERT INTO `host_instance` (`host_id`, `host_ip`, `host_name`, `host_type`, `host_project`, `is_remove`, `comment`) VALUES
-	(1, '127.0.0.1', '测试主机', '1', '1', NULL, NULL);
+INSERT INTO `host_instance` (`host_id`, `host_ip`, `host_name`, `host_port`, `host_type_key`, `host_project`, `is_remove`, `comment`) VALUES
+	(1, '192.168.0.100', '测试主机', 22, 'docker', '1', NULL, NULL);
 /*!40000 ALTER TABLE `host_instance` ENABLE KEYS */;
+
+-- 导出  表 xlauto.host_users 结构
+DROP TABLE IF EXISTS `host_users`;
+CREATE TABLE IF NOT EXISTS `host_users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `host_id` int(11) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `user_pass` varchar(50) NOT NULL,
+  `user_role` varchar(50) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- 正在导出表  xlauto.host_users 的数据：~1 rows (大约)
+/*!40000 ALTER TABLE `host_users` DISABLE KEYS */;
+INSERT INTO `host_users` (`user_id`, `host_id`, `user_name`, `user_pass`, `user_role`) VALUES
+	(1, 1, 'root', '661602611BD6C5A7B531121B4BCC8535', 'root');
+/*!40000 ALTER TABLE `host_users` ENABLE KEYS */;
 
 -- 导出  表 xlauto.projects 结构
 DROP TABLE IF EXISTS `projects`;
@@ -51,7 +69,7 @@ CREATE TABLE IF NOT EXISTS `projects` (
   PRIMARY KEY (`project_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- 正在导出表  xlauto.projects 的数据：~1 rows (大约)
+-- 正在导出表  xlauto.projects 的数据：~0 rows (大约)
 /*!40000 ALTER TABLE `projects` DISABLE KEYS */;
 INSERT INTO `projects` (`project_id`, `project_name`, `project_code`, `controller_ip`, `order_id`, `is_remove`, `create_time`, `modify_time`, `comments`) VALUES
 	(1, '湖北hk机房', 'bq01', '19.25.6.7', NULL, NULL, NULL, NULL, NULL);
@@ -92,13 +110,15 @@ CREATE TABLE IF NOT EXISTS `sys_code` (
   `order_queue` smallint(6) DEFAULT NULL,
   `comments` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`code_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
 
--- 正在导出表  xlauto.sys_code 的数据：~2 rows (大约)
+-- 正在导出表  xlauto.sys_code 的数据：~4 rows (大约)
 /*!40000 ALTER TABLE `sys_code` DISABLE KEYS */;
 INSERT INTO `sys_code` (`code_id`, `code_key`, `code_name`, `code_type`, `f_code`, `order_queue`, `comments`) VALUES
 	(1, 'docker', '容器', 'host_type', NULL, NULL, NULL),
-	(2, 'nginx', 'Nginx', 'host_type', NULL, NULL, NULL);
+	(2, 'nginx', 'Nginx', 'host_type', NULL, NULL, NULL),
+	(3, 'root', '管理员', 'user_role_type', NULL, NULL, NULL),
+	(4, 'soft', '软件用户', 'user_role_type', NULL, NULL, NULL);
 /*!40000 ALTER TABLE `sys_code` ENABLE KEYS */;
 
 -- 导出  表 xlauto.sys_menu 结构
