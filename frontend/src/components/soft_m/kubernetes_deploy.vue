bbbd4d737b18e05abe272591b254a6c929ff8dc0
wuli.wang <liww@cenboomh.com>
2020-07-29 22:15:31 +0800

    noissue;Kubernetes部署构思

--- frontend/src/components/soft_m/kubernetes_deploy.vue
+++ frontend/src/components/soft_m/kubernetes_deploy.vue
@@ -21,7 +21,7 @@
     </el-select>
     <el-button type="primary" size="mini">过滤</el-button>
 
-    <el-button type="primary" size="mini">配置</el-button>
+    <el-button type="primary" size="mini" @click="configuration_dialog = true">配置</el-button>
     <el-button type="danger" size="mini">卸载</el-button>
     <el-button type="warning" size="mini">部署</el-button>
     <el-table
@@ -56,6 +56,45 @@
         show-overflow-tooltip>
       </el-table-column>
     </el-table>
+
+    <el-dialog title="配置" :visible.sync="configuration_dialog">
+      <div style="height: 60vh; overflow: auto">
+        <el-tabs v-model="activeName" @tab-click="configurationHandleclick">
+          <el-tab-pane label="通用配置" name="configuration_general">
+            <el-form :model="configuration_form" size="mini" :inline="true">
+              <el-form-item label="防火墙">
+                <el-switch
+                  v-model="configuration_form.firewalld"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949">
+                </el-switch>
+              </el-form-item>
+              <el-form-item label="SELinux">
+                <el-switch
+                  v-model="configuration_form.selinux"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949">
+                </el-switch>
+              </el-form-item>
+              <el-form-item label="活动区域">
+                <el-select v-model="configuration_form.region" placeholder="请选择活动区域">
+                  <el-option label="区域一" value="shanghai"></el-option>
+                  <el-option label="区域二" value="beijing"></el-option>
+                </el-select>
+              </el-form-item>
+            </el-form>
+          </el-tab-pane>
+          <el-tab-pane label="Master配置" name="second">配置管理</el-tab-pane>
+          <el-tab-pane label="Node配置" name="third">角色管理</el-tab-pane>
+          <el-tab-pane label="其他" name="fourth">其他</el-tab-pane>
+        </el-tabs>
+      </div>
+      <div slot="footer" class="dialog-footer">
+          <el-button @click="dialogFormVisible = false" size="mini">取 消</el-button>
+          <el-button type="primary" @click="dialogFormVisible = false" size="mini">确 定</el-button>
+      </div>
+    </el-dialog>
+
   </el-row>
 </template>
 
@@ -64,6 +103,12 @@ export default {
   name: 'soft_deploy',
   data () {
     return {
+      activeName: 'configuration_general',
+      configuration_dialog: false,
+      configuration_form: {
+        firewalld: false,
+        selinux: false
+      },
       options: [{
         value: '选项1',
         label: '黄金糕'
@@ -115,6 +160,9 @@ export default {
   methods: {
     handleSelectionChange (val) {
       this.multipleSelection = val
+    },
+    configurationHandleclick (tab, event) {
+      console.log(tab, event)
     }
   }
 }
