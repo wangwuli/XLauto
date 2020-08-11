07f4b90f72b0d4b6eb68c5852196c6705f700822
wuli.wang <282621346@qq.com>
2020-08-11 20:07:31 +0800

    noissue;K8S部署页面构思

---
+++ main/test/test.py
@@ -0,0 +1,38 @@
+import re
+
+from src.general.Connect_G import Sshmet
+
+ssh = Sshmet()
+
+a = {
+    "host_ip": "192.168.20.163",
+    "user_name": "root",
+    "user_pass": "thzk211!",
+    "host_port": "22",
+}
+
+def console(text):
+    print(text)
+
+#
+ssh.set_info(a)
+c = ssh.connect()
+stdin, stdout, stderr = ssh.runcrealtime("llping 192.168.10.134", console)
+
+
+# import paramiko
+#
+# ssh = paramiko.SSHClient()
+# ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+# host = '9.10.11.12'
+# port, user, password = 22, 'usr', 'pass'
+# ssh.connect(host, port,  user, password)
+# stdin,stdout,stderr = c.exec_command("ping 192.168.1.1")
+#
+# for line in iter(stdout.readline,""):
+#     print(line)
+
+c.close()
+
+
+
